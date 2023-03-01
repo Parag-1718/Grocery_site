9957025419c8d99a5c9fce14ecfb61fba43d4e68
Parag-1718 <paragp022@gmail.com>
2023-03-01 16:14:52 +0530

    'initail'

---
+++ Grocery_site/src/app/front/catalogue/product-detalis/product-detalis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detalis',
+  templateUrl: './product-detalis.component.html',
+  styleUrls: ['./product-detalis.component.css']
+})
+export class ProductDetalisComponent {
+
+}
