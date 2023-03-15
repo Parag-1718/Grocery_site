dddc1cf65894019f627ca421b5da38b581be6eef
Parag-1718 <paragp022@gmail.com>
2023-03-15 11:17:00 +0530

    'update'

--- Grocefy/src/app/front/catalogue/catalogue.module.ts
+++ Grocefy/src/app/front/catalogue/catalogue.module.ts
@@ -4,14 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CatalogueRoutingModule } from './catalogue-routing.module';
 import { CategoryComponent } from './category/category.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ProductListComponent } from './product-list/product-list.component';
+
 
 
 @NgModule({
   declarations: [
     
     ProductDetailsComponent,
-    ProductListComponent
   ],
   imports: [
     CommonModule,
--- Grocefy/src/app/front/catalogue/product-list/product-list.component.ts
+++ Grocefy/src/app/front/catalogue/product-list/product-list.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  featuredProducts=[
+    {source:'assets/product/p2.png' , name:'Potatos 1kg' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'14,99'},
+    {source:'assets/product/p1.png' , name:'Redish 500g' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'9,99'},
+    {source:'assets/product/p3.png' , name:'Potatos 1kg' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'14,99'},
+    {source:'assets/product/p4.png' , name:'Tomatos200g' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'12,99'},
+    {source:'assets/product/p3.png' , name:'Broccoli 1kg' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'10,99'},
+    {source:'assets/product/p2.png' , name:'Redish 500g' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'9,99'},
+    {source:'assets/product/p1.png' , name:'Beans 250g' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'19,99'},
+    {source:'assets/product/p5.png' , name:'Redish 500g' , category:'Vegetables' , rater:'by DadaNiWadi' , price:'9,99'}
+  ]
 }
