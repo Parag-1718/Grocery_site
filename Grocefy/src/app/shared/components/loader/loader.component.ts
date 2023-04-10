340abc30e3d62fbdfda5b4bee425d902e1c49b5a
Parag-1718 <paragp022@gmail.com>
2023-04-10 13:10:40 +0530

    '90%'

---
+++ Grocefy/src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgSpinKitModule } from 'ng-spin-kit'
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent {
+
+}
